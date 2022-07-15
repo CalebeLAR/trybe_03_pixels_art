9a4bddf34440337a3daa0d7c90e9f84f1fccd714
CalebeLAR <ca6ele@gmail.com>
2022-07-15 17:24:53 -0400

    reconfigurando o github

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let listaDeCores = ["red", "green", "blue", "yellow"]
-let colorPalette = document.getElementById('color-palette');
-for (cor of listaDeCores ){
-    let div = document.createElement('div');
-    div.classList.add("color");  //class = "color"
-    div.id = cor;//class = "color cor"
-    div.style.backgroundColor = cor
-    colorPalette.appendChild(div)
+//requisitos 1, 2 e 3.
+function criaCores (array) {
+    let colorPalette = document.getElementById('color-palette');
+    for (cor of listaDeCores ){
+        let div = document.createElement('div');
+        div.classList.add("color");  //class = "color"
+        div.id = cor;//class = "color cor"
+        div.style.backgroundColor = cor
+        colorPalette.appendChild(div)
+    }
 }
+let listaDeCores = ["black","red", "green", "yellow"]
+criaCores(listaDeCores);
 
