b6e68dac7d6649a457906a9cd71d87f727ded75e
CalebeLAR <ca6ele@gmail.com>
2022-07-16 20:56:32 -0400

    requisito 4 testado e concluído

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //requisitos 1, 2 e 3.
 function criaCores (array) {
-    let colorPalette = document.getElementById('color-palette');
+    let colorPalette = document.getElementById("color-palette");
     for (cor of listaDeCores ){
-        let div = document.createElement('div');
+        let div = document.createElement("div");
         div.classList.add("color");  //class = "color"
         div.id = cor;//class = "color cor"
         div.style.backgroundColor = cor
@@ -11,4 +11,18 @@ function criaCores (array) {
 }
 let listaDeCores = ["black","red", "green", "yellow"]
 criaCores(listaDeCores);
+// requisito 4, 5, 6, 7 e 8
+let pixelBoard = document.getElementById("pixel-board");
+let comprimento = 5;
+let altura = 5;
 
+for (let al = 0; al < altura ; al += 1) {
+    let linha = document.createElement("div");
+    linha.id = "linha"
+    for (let cp = 0; cp < comprimento ; cp += 1) {
+        let pixel = document.createElement("div");
+        pixel.classList.add("pixel");
+        linha.appendChild(pixel);
+    }
+    pixelBoard.appendChild(linha);
+}
